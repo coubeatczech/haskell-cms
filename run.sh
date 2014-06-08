fb00af828d02a6ac68ca468aa689680c5bd08be1
Jakub Ryška <ryskajakub@seznam.cz>
2014-06-08 09:11:04 +0200

    javascript in the project #3

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+hastec ./src/client/*.hs 
+mkdir -p dist/
+mv ./src/client/*.js dist/
+rm ./src/client/*.hi
+rm ./src/client/*.o
+cp ./src/static/* dist/
+
 cabal install
+cd dist/
 haskell-cms -p 8000
 
